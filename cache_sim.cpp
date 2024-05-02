9bb7308b455ada1cec11eb971d8a9974832fb9ae
Thomas M <zaz22@txstate.edu>
2024-05-01 19:50:45 -0500

    linked files together, set up file output

--- cache_sim.cpp
+++ cache_sim.cpp
@@ -1,4 +1,6 @@
 #include "cache_sim.h"
+#include "cache.cpp"
+
 #include <iostream>
 #include <fstream>
 #include <cstring>
@@ -6,37 +8,38 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    ofstream output, input;
+    ofstream output;
+    ifstream input;
     input.open("memory_reference_file");
 
-    int num_entries = stoi(argv[1]);
+    int numEntries = stoi(argv[1]);
     int associativity = stoi(argv[2]);
 
     // termates if input requirements are not met
-    if ((num_entries % 2 != 0) || (num_entries % associativity != 0))
+    if ((numEntries % 2 != 0) || (numEntries % associativity != 0))
     {
         cout << "Invalid input: please review input requirements.\n";
         return -1;
     }
 
-    /*
-    TODO: file output to be added later on, implementation in another class
-    kept for reminder
-
-    output.open("cache_sim_output");
-    output.close();
-    */
     input.close();
     return 0;
 }
 
 /*
- * outputTable: function which prints out output table to console
+ * outputTable: function which prints out output table to output file
  *
  * @param reference file's data
  */
-void outputTable(char *arr[])
+void outputTable(char *arr[], ofstream out, int entries)
 {
-    cout << "";
+    CacheSim cache(entries);
+    out << "ADDR : HIT/MISS\n";
+    for (int curr = 0; curr++; curr <= entries)
+    {
+
+        out << ": ";
+    }
+
     return;
 }
