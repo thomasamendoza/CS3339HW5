e0dd1fe3e8931a11466053cae61280d2dcfb2bea
Thomas M <zaz22@txstate.edu>
2024-05-01 19:26:28 -0500

    comments added

--- cache.cpp
+++ cache.cpp
@@ -6,15 +6,30 @@ using namespace std;
 class CacheSim
 {
 private:
+    // opted for a vector so size could be mutatable
     vector<Entry> cache;
     int numEntries;
 
 public:
+    /*
+     * CacheSim: default constructor
+     *
+     * @param entries: number of entries passed in
+     */
     CacheSim(int entries)
     {
         numEntries = entries;
     }
 
+    /*
+     * access: function to simulate operating on the cache, including hit/mass
+     *         functionality
+     *
+     * @param address: address of the cache entry, implemented as an int
+     *                 per the assignment description
+     *
+     * @return: bool to signify whether the access was a hit or miss
+     */
     bool access(int address)
     {
         int index = address % numEntries;
